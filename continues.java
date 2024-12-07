60fa81826baaaeb6d61403dc02179de9630b7324
anushastefyj <24hr5a3001@mtieat.org>
2024-12-07 12:27:36 +0530

    Add files via upload

---
+++ continues.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class continues
+{
+    public static void main(String args[])
+    {
+        System.out.println("continues");
+        for(int i=0;i<5;i++)
+        {
+            if(i==2)
+            continue;
+           System.out.println(i);                                                                                                                                                            
+        }
+    }
+}
