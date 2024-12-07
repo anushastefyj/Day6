a9ad8ebb4b832981319d728c8812f98511ee7662
anushastefyj <24hr5a3001@mtieat.org>
2024-12-07 12:27:23 +0530

    Add files via upload

---
+++ Breakk.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Breakk
+{
+    public static void main(String args[])
+    {
+        System.out.println("break");
+        for(int i=0;i<5;i++)
+        {
+            if(i==2)
+            break;
+           System.out.println(i);                                                                                                                                                            
+        }
+    }
+}
